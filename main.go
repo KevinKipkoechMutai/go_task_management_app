1f67b130c7958b558cd9d5a5d3928184dfd466db
kevinmutai <mutaikevinkip.05@gmail.com>
2024-07-30 13:37:46 +0300

    Revert "edit routing"
    This reverts commit 3accde06905381b8f1dd080dad79ea3c646b2540.
    
    undo the commit to edit routing

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	// "github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -15,26 +16,27 @@ import (
 )
 
 type Todo struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Completed bool               `json:"completed"`
-	Body      string             `json:"body"`
+	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Completed bool `json:"completed"`
+	Body string `json:"body"`
 }
 
 var collection *mongo.Collection
 
-func main() {
-	fmt.Println("hello world")
+func main()  {
+	
 
 	if os.Getenv("ENV") != "production" {
-		// Load the .env file if not in production
 		err := godotenv.Load(".env")
 		if err != nil {
-			log.Fatal("Error loading .env file:", err)
+			log.Fatal("Error loading .env file")
 		}
 	}
 
-	MONGODB_URI := os.Getenv("MONGODB_URI")
-	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	
+
+	MONGO_URI := os.Getenv("MONGO_URI")
+	clientOptions := options.Client().ApplyURI(MONGO_URI)
 	client, err := mongo.Connect(context.Background(), clientOptions)
 
 	if err != nil {
@@ -44,19 +46,20 @@ func main() {
 	defer client.Disconnect(context.Background())
 
 	err = client.Ping(context.Background(), nil)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Connected to MONGODB ATLAS")
-
 	collection = client.Database("golang_db").Collection("todos")
 
-	app := fiber.New()
+	fmt.Println("Connected to mongodb")
 
+	app := fiber.New()
+	
 	// app.Use(cors.New(cors.Config{
 	// 	AllowOrigins: "http://localhost:5173",
-	// 	AllowHeaders: "Origin,Content-Type,Accept",
+	// 	AllowHeaders: "Origin, Content-Type, Accept",
 	// }))
 
 	app.Get("/api/todos", getTodos)
@@ -70,16 +73,15 @@ func main() {
 	}
 
 	if os.Getenv("ENV") == "production" {
-		app.Static("/", "./client/dist")
+		app.Static("/", "/client/dist")
 	}
 
-	log.Fatal(app.Listen("0.0.0.0:" + port))
+	log.Fatal(app.Listen("0.0.0.0:"+port))
 
 }
 
 func getTodos(c *fiber.Ctx) error {
-	var todos []Todo
-
+	var todos []Todo 
 	cursor, err := collection.Find(context.Background(), bson.M{})
 
 	if err != nil {
@@ -95,20 +97,18 @@ func getTodos(c *fiber.Ctx) error {
 		}
 		todos = append(todos, todo)
 	}
-
 	return c.JSON(todos)
 }
 
 func createTodo(c *fiber.Ctx) error {
 	todo := new(Todo)
-	// {id:0,completed:false,body:""}
-
+	
 	if err := c.BodyParser(todo); err != nil {
 		return err
 	}
 
 	if todo.Body == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "Todo body cannot be empty"})
+		return c.Status(400).JSON(fiber.Map{"error": "body cannot be empty"})
 	}
 
 	insertResult, err := collection.InsertOne(context.Background(), todo)
@@ -123,33 +123,34 @@ func createTodo(c *fiber.Ctx) error {
 
 func updateTodo(c *fiber.Ctx) error {
 	id := c.Params("id")
+
 	objectID, err := primitive.ObjectIDFromHex(id)
 
 	if err != nil {
 		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
 	}
 
-	filter := bson.M{"_id": objectID}
-	update := bson.M{"$set": bson.M{"completed": true}}
+	filter := bson.M{"_id":objectID}
+	update := bson.M{"$set":bson.M{"completed":true}}
 
 	_, err = collection.UpdateOne(context.Background(), filter, update)
+
 	if err != nil {
 		return err
 	}
-
 	return c.Status(200).JSON(fiber.Map{"success": true})
-
 }
 
 func deleteTodo(c *fiber.Ctx) error {
 	id := c.Params("id")
-	objectID, err := primitive.ObjectIDFromHex(id)
 
+	objectID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+		return c.Status(400).JSON(fiber.Map{"error": "invalid id"})
 	}
 
-	filter := bson.M{"_id": objectID}
+	filter := bson.M{"_id":objectID}
+
 	_, err = collection.DeleteOne(context.Background(), filter)
 
 	if err != nil {
